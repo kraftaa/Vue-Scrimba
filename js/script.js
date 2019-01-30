08d79a7aa2fb1c619249dc1bbb0057957e1d57ad
Maria Dubyaga <kraftaa@gmail.com>
2019-01-30 10:14:01 -0500

    changing classes by click

--- js/script.js
+++ js/script.js
@@ -44,40 +44,103 @@ function emojify(name) {
 //     }
 // })
 
+// var app = new Vue({
+//     el: "#app",
+//     data: {
+//         active: emojify("sirius--man"),
+//         // sirius is an object that contains two states: man and dog
+//         sirius: {
+//             man: emojify("sirius--man"),
+//             dog: emojify("sirius--dog")
+//         }
+//     },
+//     methods: {
+//         // an animagus is a wizard whom can shapeshift
+//         animagus: function () {
+//             this.active = (
+//                 this.active == this.sirius.man ?
+//                     this.sirius.dog :
+//                     this.sirius.man
+//             )
+//         },
+//         // breathe returns the corresponding .breathe--*
+//         breathe: function () {
+//             return (
+//                 this.active == this.sirius.man ?
+//                     "breathe--man" :
+//                     "breathe--dog"
+//             )
+//         },
+//         // background returns the corresponding background
+//         background: function () {
+//             return (
+//                 this.active == this.sirius.man ?
+//                     "" :
+//                     "black"
+//             )
+//         },
+//     }
+// })
+
+// rand returns a random item from an array
+function rand(...arr) {
+    var x = Math.floor(Math.random() * arr.length)
+    return arr[x]
+}
+
 var app = new Vue({
-    el: "#app",
+    el: ".app",
     data: {
-        active: emojify("sirius--man"),
-        // sirius is an object that contains two states: man and dog
-        sirius: {
-            man: emojify("sirius--man"),
-            dog: emojify("sirius--dog")
-        }
+        cat: emojify("box"),
+        alive_cats: [
+            emojify("cat--smile"),
+            emojify("cat--cheer"),
+            emojify("cat--laugh"),
+            emojify("cat--love" ),
+            emojify("cat--smirk"),
+            emojify("cat--kiss" ),
+            emojify("cat--fear" ),
+            emojify("cat--sad"  ),
+            emojify("cat--mad"  )
+        ],
+        dead_cats: [
+            emojify("crossbones"),
+            emojify("skull")
+        ],
     },
     methods: {
-        // an animagus is a wizard whom can shapeshift
-        animagus: function () {
-            this.active = (
-                this.active == this.sirius.man ?
-                    this.sirius.dog :
-                    this.sirius.man
-            )
+        // is_open returns whether the box is open
+        is_open: function () {
+            return this.cat != emojify("box")
         },
-        // breathe returns the corresponding .breathe--*
-        breathe: function () {
+        // is_alive returns whether the cat is alive
+        is_alive: function () {
             return (
-                this.active == this.sirius.man ?
-                    "breathe--man" :
-                    "breathe--dog"
+                this.cat != emojify("crossbones") &&
+                this.cat != emojify("skull"     )
             )
         },
-        // background returns the corresponding background
-        background: function () {
-            return (
-                this.active == this.sirius.man ?
-                    "" :
-                    "black"
+        // quantum_fluctuation observes whether the cat is alive or dead
+        quantum_fluctuation: function () {
+            if (this.is_open()) {
+                this.cat = emojify("box")
+                return
+            }
+            this.cat = rand(
+                rand(...this.alive_cats),
+                rand(...this.dead_cats ),
             )
         },
+        jittering: function () {
+            return {
+                jitter: this.is_alive()
+            }
+        },
+        theme: function () {
+            return {
+                "theme--alive" : this.is_open() && this.is_alive(),
+                "theme--dead"  : !this.is_alive()
+            }
+        }
     }
 })
