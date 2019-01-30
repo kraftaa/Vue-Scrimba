1a76719d6bb01a2de6ebdc5023f20fdf621e5062
Maria Dubyaga <kraftaa@gmail.com>
2019-01-30 09:47:23 -0500

    reusable components

--- js/script.js
+++ js/script.js
@@ -7,39 +7,39 @@ function emojify(name) {
 }
 
 // cast returns a spell (function) that decorates the wizard
-// function cast(emoji) {
-//     if (!emoji) {
-//         emoji = "¯\\_(ツ)_/¯"
-//     }
-//     return function (wizard) {
-//         return emoji + wizard + emoji
-//     }
-// }
+function cast(emoji) {
+    if (!emoji) {
+        emoji = "¯\\_(ツ)_/¯"
+    }
+    return function (wizard) {
+        return emoji + wizard + emoji
+    }
+}
 
-Vue.component("harry", {
-    template: `<p>` + emojify("harry") + `</p>`
+// Vue.component("harry", {
+//     template: `<p>` + emojify("harry") + `</p>`
+// })
+// Vue.component("ron"     , { template: `<p>` + emojify("ron"     ) + `</p>` })
+// Vue.component("hermione", { template: `<p>` + emojify("hermione") + `</p>` })
+
+Vue.component("wizard", {
+    props: ["name", "cast"],
+    template: `<p v-html="cast(name)"><slot></slot></p>`
 })
 
 var app = new Vue({
     el: "#app",
     data: {
-        wizard      : "",
-        harry       : emojify("harry"      ),
-        hedwig      : emojify("hedwig"     ),
-        ron         : emojify("ron"        ),
-        scabbers    : emojify("scabbers"   ),
-        hermione    : emojify("hermione"   ),
-        crookshanks : emojify("crookshanks")
+        harry    : emojify("harry"   ),
+        ron      : emojify("ron"     ),
+        hermione : emojify("hermione")
     },
     methods: {
-        wizards: function () {
-            return [
-                { name: this.harry   , pet: this.hedwig      },
-                { name: this.ron     , pet: this.scabbers    },
-                { name: this.hermione, pet: this.crookshanks }
-            ]
-        }
+        // oculus_reparo returns a spell (function) that repairs glasses
+        oculus_reparo: cast(emojify("oculus-reparo")),
+        // wingardium_leviosa returns a spell (function) that levitates an object
+        wingardium_leviosa: cast(emojify("wingardium-leviosa")),
+        // alohomora returns a spell (function) that unlocks a door
+        alohomora: cast(emojify("alohomora"))
     }
 })
-
-app.wizard = app.ron
