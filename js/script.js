af50e7788e405800164f33280717e43ec568f831
Maria Dubyaga <kraftaa@gmail.com>
2019-01-30 06:45:24 -0500

    moving script to js file

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+// emojify returns the corresponding emoji image
+function emojify(name) {
+    var out = `<img src="emojis/` + name + `.png">`
+    return out
+}
+
+// cast returns a spell (function) that decorates the wizard
+function cast(emoji) {
+    if (!emoji) {
+        emoji = "¯\\_(ツ)_/¯"
+    }
+    return function (wizard) {
+        return emoji + wizard + emoji
+    }
+}
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        wizard: emojify("wizard")
+    }
+})
+
