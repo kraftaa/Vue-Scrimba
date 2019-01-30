34111bde17cd575a6312636813cab160d3a3c43d
Maria Dubyaga <kraftaa@gmail.com>
2019-01-30 10:29:49 -0500

    grid colors

--- js/script.js
+++ js/script.js
@@ -83,63 +83,120 @@ function emojify(name) {
 // })
 
 // rand returns a random item from an array
-function rand(...arr) {
-    var x = Math.floor(Math.random() * arr.length)
-    return arr[x]
-}
+// function rand(...arr) {
+//     var x = Math.floor(Math.random() * arr.length)
+//     return arr[x]
+// }
+
+// var app = new Vue({
+//     el: ".app",
+//     data: {
+//         cat: emojify("box"),
+//         alive_cats: [
+//             emojify("cat--smile"),
+//             emojify("cat--cheer"),
+//             emojify("cat--laugh"),
+//             emojify("cat--love" ),
+//             emojify("cat--smirk"),
+//             emojify("cat--kiss" ),
+//             emojify("cat--fear" ),
+//             emojify("cat--sad"  ),
+//             emojify("cat--mad"  )
+//         ],
+//         dead_cats: [
+//             emojify("crossbones"),
+//             emojify("skull")
+//         ],
+//     },
+//     methods: {
+//         // is_open returns whether the box is open
+//         is_open: function () {
+//             return this.cat != emojify("box")
+//         },
+//         // is_alive returns whether the cat is alive
+//         is_alive: function () {
+//             return (
+//                 this.cat != emojify("crossbones") &&
+//                 this.cat != emojify("skull"     )
+//             )
+//         },
+//         // quantum_fluctuation observes whether the cat is alive or dead
+//         quantum_fluctuation: function () {
+//             if (this.is_open()) {
+//                 this.cat = emojify("box")
+//                 return
+//             }
+//             this.cat = rand(
+//                 rand(...this.alive_cats),
+//                 rand(...this.dead_cats ),
+//             )
+//         },
+//         jittering: function () {
+//             return {
+//                 jitter: this.is_alive()
+//             }
+//         },
+//         theme: function () {
+//             return {
+//                 "theme--alive" : this.is_open() && this.is_alive(),
+//                 "theme--dead"  : !this.is_alive()
+//             }
+//         }
+//     }
+// })
+
+Vue.component("swatch", {
+    props: ["active", "swatch", "effect"],
+    template: `
+        <div class="grid-item">
+            <div class="grid-cell--top" :style="effect(swatch)" >
+                <span
+                    v-html="swatch.emoji"
+                    :class="{ bounce: swatch == active }"
+                ></span>
+            </div>
+            <div class="grid-cell--bottom" :style="corrected_color()">
+                {{ swatch.color.toUpperCase() }}
+            </div>
+        </div>
+    `,
+    methods: {
+        corrected_color: function (swatch) {
+            return {
+                color: this.swatch.color == '#ffffff' ? '#000000' : this.swatch.color
+            }
+        }
+    }
+})
 
 var app = new Vue({
-    el: ".app",
+    el: "#app",
     data: {
-        cat: emojify("box"),
-        alive_cats: [
-            emojify("cat--smile"),
-            emojify("cat--cheer"),
-            emojify("cat--laugh"),
-            emojify("cat--love" ),
-            emojify("cat--smirk"),
-            emojify("cat--kiss" ),
-            emojify("cat--fear" ),
-            emojify("cat--sad"  ),
-            emojify("cat--mad"  )
-        ],
-        dead_cats: [
-            emojify("crossbones"),
-            emojify("skull")
+        active: "",
+        swatches: [
+            { emoji: emojify("lion"    ), color: "#ff691f" },
+            { emoji: emojify("tiger"   ), color: "#fab81e" },
+            { emoji: emojify("fish"    ), color: "#7fdbb6" },
+            { emoji: emojify("frog"    ), color: "#19cf86" },
+            { emoji: emojify("dolphin" ), color: "#91d2fa" },
+            { emoji: emojify("whale"   ), color: "#1b95e0" },
+            { emoji: emojify("elephant"), color: "#abb8c2" },
+            { emoji: emojify("octopus" ), color: "#e81c4f" },
+            { emoji: emojify("pig"     ), color: "#f58ea8" },
+            { emoji: emojify("unicorn" ), color: "#981ceb" },
+            { emoji: emojify("rabbit"  ), color: "#ffffff" },
+            { emoji: emojify("wolf"    ), color: "#000000" },
         ],
     },
     methods: {
-        // is_open returns whether the box is open
-        is_open: function () {
-            return this.cat != emojify("box")
+        // activate actives a swatch (emoji/color)
+        activate: function (swatch) {
+            this.active = swatch
         },
-        // is_alive returns whether the cat is alive
-        is_alive: function () {
-            return (
-                this.cat != emojify("crossbones") &&
-                this.cat != emojify("skull"     )
-            )
-        },
-        // quantum_fluctuation observes whether the cat is alive or dead
-        quantum_fluctuation: function () {
-            if (this.is_open()) {
-                this.cat = emojify("box")
-                return
-            }
-            this.cat = rand(
-                rand(...this.alive_cats),
-                rand(...this.dead_cats ),
-            )
-        },
-        jittering: function () {
+        // gradient returns a precomposed gradient
+        gradient: function (swatch) {
             return {
-                jitter: this.is_alive()
-            }
-        },
-        theme: function () {
-            return {
-                "theme--alive" : this.is_open() && this.is_alive(),
-                "theme--dead"  : !this.is_alive()
+                background: "linear-gradient(100deg, whitesmoke -100%, " + swatch.color + ")",
             }
         }
     }
